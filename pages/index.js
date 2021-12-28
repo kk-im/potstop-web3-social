52e7bd48ea1dd986303040be9e95976681381f5c
kk-im <kellykim0996@gmail.com>
2021-12-27 23:58:33 -0600

    debug 1

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/jsx-key */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react-hooks/exhaustive-deps */
 // for next.js's <head> tag and rendering images
 import Head from 'next/head'
 import Image from 'next/image'
